828cf55903c7209221bed72c9a6c5bf0a41ae05d
John-Arboleda <johnjack0126@gmail.com>
2021-10-02 20:27:23 -0500

    Add opinion view partial, index method

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -2,15 +2,17 @@ class OpinionsController < ApplicationController
   before_action :require_user
 
   def index
+    @opinion = Opinion.new
+  	@opinions = Opinion.order_by_most_recent
   end
-  
+
   def create
   	@opinion = current_user.opinions.build(opinion_params)
   	if @opinion.save
   	  flash[:notice] = 'Opinion was created successfully'
       redirect_to root_path
   	else
-  	  flash.new[:alert] = 'Something went wrong...'
+  	  flash.now[:alert] = 'Something went wrong...'
   	end
   end
 
