2b965f5fc8967fa63949af92bc8fe51d1393f868
John-Arboleda <johnjack0126@gmail.com>
2021-11-05 10:41:49 -0500

    Add comments view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'opinions#index'
   resources :users, only: %i[new create edit update]
+  resources :opinions, only: [:index, :create] do
+  	resources :comments, only: [:create]
+  end
   get 'sessions/new'
   get 'sign_in', to: 'sessions#new'
   post 'sign_in', to: 'sessions#create'
