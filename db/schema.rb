cc1e0eff7d39b7aa43bb3df2c9d1e05032f6ae73
John-Arboleda <johnjack0126@gmail.com>
2021-11-05 07:45:47 -0500

    Migrate Comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_03_164619) do
+ActiveRecord::Schema.define(version: 2021_11_05_123750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "opinion_id"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "followings", force: :cascade do |t|
     t.integer "follower_id", null: false
